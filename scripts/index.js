60939ac30232a030cf5a380ce1138bcb088099fa
nahtigal <petryk_roman@ukr.net>
2015-12-15 18:37:41 +0000

    icons aded

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,22 @@
 $(function(){
   $.getJSON('http://api.openweathermap.org/data/2.5/'+
-                    'forecast/daily?q=Lima&units=metric&cnt=3&appid=d97e90c822543d704b84ea0fc0900327', 
+                    'forecast/daily?q=Montevideo&units=metric&cnt=3&appid=d97e90c822543d704b84ea0fc0900327', 
       function (data){
       $('#tToday').html(data.list[0].temp.day);
       $('#tTomorrow').html(data.list[1].temp.day);
       $('#tAfterTomorrow').html(data.list[2].temp.day);
+      
+      $('#pToday').html(data.list[0].pressure);
+      $('#pTomorrow').html(data.list[1].pressure);
+      $('#pAfterTomorrow').html(data.list[2].pressure);
+      
+      
+      
+      $('#iconToday').html('<img src="images/'+ data.list[0].weather[0].icon+ '.png" alt="Weather icon">');
+      $('#iconTomorrow').html('<img src="images/'+ data.list[1].weather[0].icon+ '.png" alt="Weather icon">');
+      $('#iconAfterTomorrow').html('<img src="images/'+ data.list[2].weather[0].icon+ '.png" alt="Weather icon">');
+      
+      $('#Place').html(data.city.name+' '+ data.city.coord.lat+'  '+data.city.coord.lon);
       }
   ); 
   
