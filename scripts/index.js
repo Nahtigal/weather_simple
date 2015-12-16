c2e6f4e19224358725e5554810fdee31da4a1f06
nahtigal <petryk_roman@ukr.net>
2015-12-16 15:42:12 +0000

    16.12.2015

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 $(function(){
   $.getJSON('http://api.openweathermap.org/data/2.5/'+
-                    'forecast/daily?q=Montevideo&units=metric&cnt=3&appid=d97e90c822543d704b84ea0fc0900327', 
+                    'forecast/daily?q=Kiev&units=metric&cnt=3&lang=ua&appid=d97e90c822543d704b84ea0fc0900327', 
       function (data){
       $('#tToday').html(data.list[0].temp.day);
       $('#tTomorrow').html(data.list[1].temp.day);
@@ -16,7 +16,13 @@ $(function(){
       $('#iconTomorrow').html('<img src="images/'+ data.list[1].weather[0].icon+ '.png" alt="Weather icon">');
       $('#iconAfterTomorrow').html('<img src="images/'+ data.list[2].weather[0].icon+ '.png" alt="Weather icon">');
       
+      $('#TextToday').html(data.list[0].weather[0].description);
+      $('#TextTomorrow').html(data.list[1].weather[0].description);
+      $('#TextAfterTomorrow').html(data.list[2].weather[0].description);
+      
+      
       $('#Place').html(data.city.name+' '+ data.city.coord.lat+'  '+data.city.coord.lon);
+      $('title').html("Погода в" + data.city.name);
       }
   ); 
   
