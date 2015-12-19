f8b8530a5628585dd34bb5633c978b2db853c011
nahtigal <petryk_roman@ukr.net>
2015-12-19 12:28:20 +0000

    19122015

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,12 @@
 $(function(){
-  $.getJSON('http://api.openweathermap.org/data/2.5/'+
-                    'forecast/daily?q=Kiev&units=metric&cnt=3&lang=ua&appid=d97e90c822543d704b84ea0fc0900327', 
+  
+  /* 
+  //$.getJSON('http://api.openweathermap.org/data/2.5/'+
+    //                'forecast/daily?q=Kiev&units=metric&cnt=3&lang=ua&appid=d97e90c822543d704b84ea0fc0900327', 
+     $.getJSON('http://api.openweathermap.org/data/2.5/find?lat=' + $('#lat').val() + '&lon='+ $('#long').val() + '&cnt=10'+
+            '&lang=ru&units=metric&appid=d97e90c822543d704b84ea0fc0900327',
       function (data){
-      $('#tToday').html(data.list[0].temp.day);
+      /*$('#tToday').html(data.list[0].temp.day);
       $('#tTomorrow').html(data.list[1].temp.day);
       $('#tAfterTomorrow').html(data.list[2].temp.day);
       
@@ -23,8 +27,30 @@ $(function(){
       
       $('#Place').html(data.city.name+' '+ data.city.coord.lat+'  '+data.city.coord.lon);
       $('title').html("Погода в" + data.city.name);
-      }
-  ); 
+      
+      $('#tToday').html(data.list[0].name);
+      $('#tTomorrow').html(data.list[1].name);
+      $('#tAfterTomorrow').html(data.list[2].name);
+        });
+  */
+$('#btnGetWeather').click(function() {
+do_something("text","text1","text2");
+});
   
+  function getWeather(lat,long){
+        $.getJSON('http://api.openweathermap.org/data/2.5/find?lat=' + lat + '&lon='+ long + '&cnt=10'+
+            '&lang=ru&units=metric&appid=d97e90c822543d704b84ea0fc0900327',
+      function (data){
+      $('#tToday').html(data.list[0].name);
+      $('#tTomorrow').html(data.list[1].name);
+      $('#tAfterTomorrow').html(data.list[2].name);
+        });
     
+  }
+  
+  function do_something(a,b,c){
+    $('#tToday').html(a);
+      $('#tTomorrow').html(b);
+      $('#tAfterTomorrow').html(c);
+  }
 });
